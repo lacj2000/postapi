0e125e1f0e08594cad0cc4756af0b607ea09c439
lacj2000 <luiz.araujofla@gmail.com>
2020-12-21 21:57:22 -0300

    correção dos models

--- post_api/post/models.py
+++ post_api/post/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
     body = models.CharField(max_length=1024)
     post_creator = models.ForeignKey('user.Profile',on_delete=models.CASCADE) 
-
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
-    comment_creator_id = models.ForeignKey('user.Profile',on_delete=models.CASCADE)
     post_id = models.ForeignKey('Post',on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
     body = models.CharField(max_length=512)
+    def __str__(self):
+        return self.name
